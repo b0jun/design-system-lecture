3d4e260267850d8c2fd84f42c06227a8a447ac98
b0jun <bjkim0228@naver.com>
2025-06-28 15:53:24 +0900

    feat: TagList

---
+++ src/stories/TagButton.stories.ts
@@ -0,0 +1,37 @@
+import type { Meta, StoryObj } from '@storybook/react-vite';
+import { fn } from 'storybook/test';
+
+import TagButton from '../components/TagButton';
+
+const meta = {
+  title: 'Buttons/TagButton',
+  component: TagButton,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  argTypes: {
+    children: {
+      control: 'text',
+      description: '버튼의 텍스트',
+    },
+    isChecked: {
+      control: 'boolean',
+      description: '버튼 활성화 여부',
+    },
+    onClick: { action: 'clicked', description: '버튼 클릭 이벤트' },
+  },
+  args: {
+    onClick: fn(),
+  },
+} satisfies Meta<typeof TagButton>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    children: 'button',
+    isChecked: false,
+  },
+};
